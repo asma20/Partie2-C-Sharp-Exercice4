1bd8fd6920d8586ab9f824ed52a26ce1f79fe8fd
asma20 <jbeli.asma20@gmail.com>
2021-02-16 14:45:15 +0100

    switch

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Partie2Exercice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String login;
+            String password;
+            Console.WriteLine("Saisir votre login");
+            login=Console.ReadLine();
+            Console.WriteLine("Saisir votre login");
+            password = Console.ReadLine();
+
+            int i = 0;
+            if(login=="asma"&& password == "123")
+            {
+                i= 1;
+            }
+            else
+            {
+                i = 0;
+            }
+
+             
+            switch (i)
+            {
+
+                
+                case 0:
+                    
+                    Console.WriteLine("Mavais login ou password .");
+                    break;
+               
+                default:
+                    Console.WriteLine("Bienvenue à la Manu.");
+                    break;
+            }
+
+           
+
+        }
+    }
+}
